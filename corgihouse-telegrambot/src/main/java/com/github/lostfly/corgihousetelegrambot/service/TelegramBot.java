c8862493528ed07ce7e9e94f6dfd4eda53f2c0fc
Liza12345677564 <lizaveprova13@gmail.com>
2023-10-17 14:16:43 +0500

    Add Keyboard menu

--- corgihouse-telegrambot/src/main/java/com/github/lostfly/corgihousetelegrambot/service/TelegramBot.java
+++ corgihouse-telegrambot/src/main/java/com/github/lostfly/corgihousetelegrambot/service/TelegramBot.java
@@ -22,6 +22,9 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScope;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
@@ -238,6 +241,26 @@ public class TelegramBot extends TelegramLongPollingBot {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSend);
+
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+
+        List<KeyboardRow> keyboardRows =new ArrayList<>();
+
+        KeyboardRow row1 = new KeyboardRow();
+
+        row1.add("Профиль");
+        row1.add("Поиск встреч");
+        keyboardRows.add(row1);
+
+        KeyboardRow row2 = new KeyboardRow();
+
+        row2.add("Питомцы");
+        row2.add("Мои встречи");
+        keyboardRows.add(row2);
+
+        keyboardMarkup.setKeyboard(keyboardRows);
+
+        message.setReplyMarkup(keyboardMarkup);
 
         try {
             execute(message);
